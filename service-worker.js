35bf77b0c7e465bff0afa087244f11427761edd1
Bryan McBride <mcbride.bryan@gmail.com>
2018-10-30 08:18:34 -0400

    Update sw

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-10.30.18.1';
+const PRECACHE = 'precache-10.30.18.2';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -27,13 +27,14 @@ const PRECACHE_URLS = [
   'assets/img/favicon-32x32.png',
   'assets/img/favicon-16x16.png',
   'assets/img/android-chrome-192x192.png',
-  'assets/leaflet-1.3.1/leaflet.css',
-  'assets/leaflet-1.3.1/leaflet.js',
-  'assets/leaflet-1.3.1/images/marker-icon.png',
-  'assets/leaflet-1.3.1/images/marker-icon-2x.png',
-  'assets/leaflet-1.3.1/images/marker-shadow.png',
-  'assets/leaflet-geopackage-2.0.4/leaflet-geopackage.min.js',
-  'merge.gpkg'
+  'assets/leaflet-1.3.4/leaflet.css',
+  'assets/leaflet-1.3.4/leaflet.js',
+  'assets/leaflet-1.3.4/images/marker-icon.png',
+  'assets/leaflet-1.3.4/images/marker-icon-2x.png',
+  'assets/leaflet-1.3.4/images/marker-shadow.png',
+  'assets/leaflet-geopackage-2.0.5/leaflet-geopackage.min.js',
+  'merge.gpkg',
+  'niskayuna_topo.gpkg'
 ];
 
 // The install handler takes care of precaching the resources we always need.
