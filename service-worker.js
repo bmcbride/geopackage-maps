c15ac03c34a0d454084437cfcf107ac4dd14ea5e
Bryan McBride <mcbride.bryan@gmail.com>
2018-10-30 08:14:19 -0400

    Updgrade Leaflet and leaflet-geopackage, add topo

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v6';
+const PRECACHE = 'precache-10.30.18.1';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -28,6 +28,7 @@ const PRECACHE_URLS = [
   'assets/img/favicon-16x16.png',
   'assets/img/android-chrome-192x192.png',
   'assets/leaflet-1.3.1/leaflet.css',
+  'assets/leaflet-1.3.1/leaflet.js',
   'assets/leaflet-1.3.1/images/marker-icon.png',
   'assets/leaflet-1.3.1/images/marker-icon-2x.png',
   'assets/leaflet-1.3.1/images/marker-shadow.png',
